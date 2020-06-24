6d7ba5d4b0d2d170528ee6f54632e0febfd20de7
SahilModak <modaksahil@gmail.com>
2020-06-24 01:01:01 -0700

    gamelogic functions added

--- app/src/main/java/com/example/zombieseeker/gamelogic.java
+++ app/src/main/java/com/example/zombieseeker/gamelogic.java
@@ -31,5 +31,46 @@ public class gamelogic {
     }
 
     public void initializeBoard(){
+        int max = cols*rows;
+        int min = 0;
+        int arr[];
+        board = new boolean[rows][cols];
+        for (int i = 0; i < num_planets; i++) {
+            int random_int = (int) (Math.random() * (max - min + 1) + min);
+            int row_index = random_int/cols;
+            board[row_index][random_int%cols] = true;
+        }
     }
+
+    public boolean checkPlanet(int row_index, int col_index){
+        return board[row_index][col_index];
+    }
+
+
+    public int checkCol(int col_index){
+        int count = 0;
+        for(int i = 0; i < rows; i++){
+            if(board[i][col_index]){
+                count = count + 1;
+            }
+        }
+        return count;
+    }
+
+    public int checkRow(int row_index){
+        int count = 0;
+        for(int i = 0; i < cols; i++){
+            if(board[row_index][i]){
+                count = count + 1;
+            }
+        }
+        return count;
+    }
+
+    public int totalPlanets(int row_index, int col_index){
+        return checkCol(col_index) + checkRow(row_index);
+    }
+
+
+
 }
