56b94afde563e2f49ddca9413681682b8883286d
smodak <smodak@sfu.ca>
2020-06-25 01:33:05 -0700

    intent added

--- app/src/main/java/com/example/zombieseeker/settings.java
+++ app/src/main/java/com/example/zombieseeker/settings.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class settings extends AppCompatActivity {
 
@@ -12,10 +14,22 @@ public class settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        setupBack();
     }
 
-    public static Intent switchOptions(Context context){
+    public static Intent switchOptions(Context context) {
         return new Intent(context, settings.class);
     }
 
+    private void setupBack() {
+
+        Button back = (Button) findViewById(R.id.optionsBtn);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = mainscreen.skipAnimation(settings.this);
+                startActivity(intent);
+            }
+        });
+    }
 }
