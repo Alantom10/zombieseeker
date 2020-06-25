b1fadbe6bedbb3943d652dbe6ea01ab9c63e1c76
Alan Thomas <acthomas2000@users.noreply.github.com>
2020-06-25 03:37:08 -0700

    recreating settings page

--- app/src/main/java/com/example/zombieseeker/settings.java
+++ app/src/main/java/com/example/zombieseeker/settings.java
@@ -7,6 +7,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class settings extends AppCompatActivity {
 
@@ -14,22 +16,43 @@ public class settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-        setupBack();
+
+//        setupBack();
+//        createBoardSizeRadioButtons();
+        createNumPlanetsRadioButtons();
     }
+
+    private void createNumPlanetsRadioButtons() {
+        RadioGroup group = (RadioGroup) findViewById(R.id.numPlanetsRadioGroup);
+
+        int[] numPlanets = getResources().getIntArray(R.array.num_of_planets);
+
+        for (int i = 0; i < numPlanets.length; i++) {
+            int numPlanet = numPlanets[i];
+
+            RadioButton button = new RadioButton(this);
+            button.setText(numPlanet + " planets");
+
+            group.addView(button);
+        }
+    }
+
+//    private void createBoardSizeRadioButtons() {
+//    }
 
     public static Intent switchOptions(Context context) {
         return new Intent(context, settings.class);
     }
 
-    private void setupBack() {
-
-        Button back = (Button) findViewById(R.id.optionsBtn);
-        back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = mainscreen.skipAnimation(settings.this);
-                startActivity(intent);
-            }
-        });
-    }
+//    private void setupBack() {
+//
+//        Button back = (Button) findViewById(R.id.optionsBtn);
+//        back.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = mainscreen.skipAnimation(settings.this);
+//                startActivity(intent);
+//            }
+//        });
+//    }
 }
