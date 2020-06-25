ffa2f0b840a709b6f14da41c90502421544b3844
Alan Thomas <acthomas2000@users.noreply.github.com>
2020-06-25 00:22:44 -0700

    Added animations.

--- app/src/main/java/com/example/zombieseeker/MainActivity.java
+++ app/src/main/java/com/example/zombieseeker/MainActivity.java
@@ -5,16 +5,56 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView logo;
+    ImageView shuttle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        logo = (ImageView) findViewById(R.id.logoImage);
+        Animation logoAnimation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.lefttoright);
+        logo.startAnimation(logoAnimation);
+
+        shuttle = (ImageView) findViewById(R.id.shuttleImage);
+        Animation shuttleAnimation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.downtoup);
+        shuttle.startAnimation(shuttleAnimation);
+
         setupSkipBtn();
 
+        shuttleAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Timer timer;
+                timer = new Timer();
+                timer.schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        Intent intent = mainscreen.skipAnimation(MainActivity.this);
+                        startActivity(intent);
+                    }
+                }, 4000);
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
     }
 
     private void setupSkipBtn() {
